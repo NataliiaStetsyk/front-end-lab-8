d372ef038a06d54f73d132b4fc077a697559649d
Nataliia <nstetsyk03@gmail.com>
2018-02-08 08:41:12 +0200

    fixes

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,17 +1,17 @@
-
 var euro = parseInt(prompt('Please, input amount of EURO'));
 var usd = parseInt(prompt('Please, input amount of USD'));
 
+if (isNaN(euro) || isNaN(usd)) {
+    console.log("Incorrect data!");
+} else {
+    var EuroToUAH = euro * 33.8565;
+    var EuroToUAH = Math.round(EuroToUAH * 100) / 100;
+    var UsdToUAH = usd * 27.4609;
+    var UsdToUAH = Math.round(UsdToUAH * 100) / 100;
 
-var EuroToUAH = euro*33.8565;
-var EuroToUAH = Math.round(EuroToUAH * 100) / 100;
-var UsdToUAH = usd*27.4609;
-var UsdToUAH = Math.round(UsdToUAH * 100) / 100;
+    var EuroToUsd = 33.8565 / 27.4609;
+    var EuroToUsd = Math.round(EuroToUsd * 100) / 100;
 
-var EuroToUsd = 33.8565/27.4609;
-var EuroToUsd = Math.round(EuroToUsd * 1000) / 1000;
+    console.log("For data" + " " + euro + "," + " " + usd + ":" + " " + euro + " " + "euros are equal" + " " + EuroToUAH + " " + "UAH," + " " + usd + " " + "dollars are equal" + " " + UsdToUAH + " " + "UAH" + ", one euro is equal" + " " + EuroToUsd + " " + "dollars.");
 
-console.log("For data" + " "+ euro+","+ " "+ usd +":"+ " "+euro+ " " + "euros are equal"+ " "+ EuroToUAH+ " "+ "UAH,"+ " "+ usd+ " "+ "dollars are equal"+ " "+ UsdToUAH + " "+"UAH" + ", one euro is equal"+" "+ EuroToUsd
- + " " + "dollars.");
-
-
+}
