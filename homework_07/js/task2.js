973c960d94f8411d021389a730f3897ca914c1a8
Nataliia <nstetsyk03@gmail.com>
2018-02-13 21:47:59 +0200

    refactoring

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,72 +1,43 @@
-var message = confirm("Do you want to play a game?");
-if (!message) {
+var start = confirm("Do you want to play a game?");
+if (!start) {
     console.log("You did not become a millionaire");
 }
-var min = 0;
-var max = 5;
-var random = Math.floor(Math.random() * ((max - min) + 1) + min);
-random = parseInt(random);
-var prize1 = 10;
-var prize2 = 5;
-var prize3 = 3;
-var prize4 = 0;
-var prize = 0;
-while (message) {
-    var attempts = 3;
-    
-    //prompt the user for a guess
-    var guess = prompt('Enter a whole number between 1 and ' + max + "\n" + 'Attempts left ' + attempts + "\n"  +"Total prize: "  + "$" + prize + "\n" + "Possible prize on current attempt: " + prize1 + "$");
-    //ensure user entered something
-    if ((guess < 0) || (guess > max) || (!guess) || (isNaN(guess))) {
-        console.log('Incorrect data!');
-    }
-    if ((guess == random) && (attempts == 3)) {
-     prize = prize1;
-    } else {
-        attempts = 2;
-        
-         guess = prompt('Enter a whole number between 1 and ' + max + "\n" + 'Attempts left: ' + attempts + "\n"  +"Total prize: "  + "$" + prize + "\n" + "Possible prize on current attempt: " + prize2 + "$");
-        if ((guess < 0) || (guess > max) || (!guess) || (isNaN(guess))) {
-            console.log('Incorrect data!');
+
+while (start) {
+    var max = 5,
+        min = 0,
+        newGame = 0,
+        total = 0,
+        prize = 10,
+        win = 10,
+        attempts = 0,
+        turns = 3,
+        askContinue = true,
+        random = Math.floor(Math.random() * ((max - min) + 1) + min); //Generate a random number
+
+    while (attempts < 3 && askContinue) {
+        var guess = prompt('Enter a whole number between 0 and ' + max + "\n" + 'Attempts left: ' + turns + "\n" + 'Total prize: ' + total + "\n" + "Possible prize on current attempt: " + win + "$");
+        if (Number(guess) === random) {
+            total += win; //Prize must be added to the previous one 
+            attempts = 0;
+            turns = 3;
+            max = max * 2; // Make number range twice as big as the previous one
+            prize *= 3; // Make three times bigger maximum prize 
+            win = prize; // prize at the current attempt
+            random = Math.floor(Math.random() * ((max - min) + 1) + min);
+            askContinue = confirm('Do you want to continue a game?');
+        } else if (!isFinite(guess) || (!guess) || isNaN(guess)) { //Ensure user entered something
+            console.log("Incorrect data! Please enter a valid number");
+            break;
+
+        } else { //User have lost
+            turns--;
+            attempts++;
+            win = Math.floor(prize / (attempts * 2) * 1) / 1;
         }
-        if ((guess == random) && (attempts == 2)) {
-             prize = prize2;
-        } else {
-            attempts = 1;
-            
-             guess = prompt('Enter a whole number between 1 and ' + max + "\n" + 'Attempts left: ' + attempts + "\n" +"Total prize: "  + "$" + prize + "\n" + "Possible prize on current attempt: " + prize3 + "$");
-            if ((guess < 0) || (guess > max) || (!guess) || (isNaN(guess))) {
-                console.log('Incorrect data!');
-            }
-            if ((guess == random) && (attempts == 1)) {
-                prize = prize3;
-            } else {
-                prize = prize4;
-					
-            }
-        }	
     }
-    prize += prize;
-	console.log("Thank you for a game. your prize is: " + prize + "$");
-    message = confirm("Do you want to continue a game?"); //	If user does want to continue, make number range twice as big as the previous one 
-    if (message) {
-         max = max * 2;
-         random = Math.floor(Math.random() * ((max - min) + 1) + min);
-         random = parseInt(random);
-         prize1 = prize1 * 3;
-         prize2 = prize2 * 3;
-         prize3 = Math.floor((prize2/2) * 1)/1;		
-    }
-    if (!message) {
-        console.log("Thank you for a game. Total prize is: " + prize + "$");
-         message = confirm("Do you want to play again?");
-         min = 0;
-         max = 5;
-         random = Math.floor(Math.random() * ((max - min) + 1) + min);
-         random = parseInt(random);
-         prize1 = 10;
-         prize2 = 5;
-         prize3 = 3;
-         prize4 = 0;
-    }
+
+    console.log("Thank you for a game. Total prize is: " + total + "$");
+    start = confirm('Do you want to try again?'); //Start a new Game
+    newGame++;
 }
