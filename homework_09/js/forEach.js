097f03093ad93014ee241e3af66b552d7f7ad7ce
Nataliia Stetsyk <34649004+NataliiaStetsyk@users.noreply.github.com>
2018-02-19 11:13:55 +0200

    Update forEach.js

--- homework_09/js/forEach.js
+++ homework_09/js/forEach.js
@@ -1,9 +1,11 @@
 function forEach(array, fn) {
     if(array && fn){
-        array.forEach(fn);
+		for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
     }
 }
 
-forEach([3, 5, 2], function (e) {
+/*forEach([3, 5, 2], function (e) {
     console.log(e)
-});
+});*/
