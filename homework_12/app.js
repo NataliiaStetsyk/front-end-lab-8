45ccc4bd10e5e9613df5ed47de04a1e5a263246c
Nataliia Stetsyk <34649004+NataliiaStetsyk@users.noreply.github.com>
2018-03-01 12:05:48 +0200

    Update app.js

--- homework_12/app.js
+++ homework_12/app.js
@@ -4,70 +4,78 @@ var page = document.createElement("div");
 page.setAttribute("class", "page");
 rootNode.appendChild(page);
 
-        var header = document.createElement('header');
-		page.appendChild(header);
-        var logo = document.createElement('h1');
-		logo.setAttribute("class", "logo");
-		header.appendChild(logo);
-        var logoText = document.createTextNode('Most popular tanks');
-        logo.appendChild(logoText);
-    
+var header = document.createElement('header');
+page.appendChild(header);
+var logo = document.createElement('h1');
+logo.setAttribute("class", "logo");
+header.appendChild(logo);
+var logoText = document.createTextNode('Most popular tanks');
+logo.appendChild(logoText);
 
 
-  tanks.forEach(function(element) {
-     var model = document.createElement('div');
-     model.innerHTML = element.model;
-     rootNode.appendChild(model);
+
+tanks.forEach(function(element) {
+    var box = document.createElement('div');
+    box.id = "box";
+    page.appendChild(box);
 
 
-     var preview = document.createElement('img');
-     preview.id = "id";
-     preview.className = "class";
-     preview.src = element.preview;
-     rootNode.appendChild(preview);
-
-     var country = document.createElement('div');
-     country.innerHTML = element.country;
-     rootNode.appendChild(country);
-
-	 var country_image = document.createElement('img');
-	 country_image.id = "id";
-     country_image.className = "class";
-     country_image.src = element.country_image;
-     rootNode.appendChild(country_image);
+    var preview = document.createElement('img');
+    preview.className = "preview_class";
+    preview.src = element.preview;
+    box.appendChild(preview);
 
 
-	 var level = document.createElement('div');
-	 level.innerHTML = element.level;
-	 rootNode.appendChild(level);
+    var caption = document.createElement('caption');
+    caption.className = "caption_class";
+    box.appendChild(caption);
 
-	 var details = document.createElement('div');
-	 details.id = "id";
-	 details.className = "class";
-	 rootNode.appendChild(details);
-     
-	 if(element.children){
-         details.forEach(function(el) {
+    var country_image = document.createElement('img');
+    country_image.id = "country_image_id";
+    country_image.className = "country_image_class";
+    country_image.src = element.country_image;
+    caption.appendChild(country_image);
+
+    var level = document.createElement('div');
+    level.className = "level";
+    level.innerHTML = element.level;
+    caption.appendChild(level);
+
+    var model = document.createElement('div');
+    model.innerHTML = element.model;
+    caption.appendChild(model);
+    model.className = "model_class";
+
+    var country = document.createElement('div');
+    country.innerHTML = element.country;
+    country.className = "country_class";
+    caption.appendChild(country);
+
+    var details = document.createElement('div');
+    details.id = "details_id";
+    details.className = "details_class";
+    caption.appendChild(details);
+
+    if (element.children) {
+        details.forEach(function(el) {
             var damage = document.createElement('div');
-	        details.appendChild(damage);
-	        damage.innerHTML = el.damage;
-	        var breoning = document.createElement('div');
-	        details.appendChild(breoning);
-	        breoning.innerHTML = el.breoning;
-	        var attack_speed = document.createElement('div');
-	        details.appendChild(attack_speed);
-	        attack_speed.innerHTML = el.attack_speed;
-	        var time_of_targeting = document.createElement('div');
-	        details.appendChild(time_of_targeting);
-	        time_of_targeting.innerHTML = el.time_of_targeting;
-	        var ammunition = document.createElement('div');
-	        details.appendChild(ammunition);
-	        ammunition.innerHTML = el.ammunition;
+            details.appendChild(damage);
+            damage.innerHTML = el.damage;
+            var breoning = document.createElement('div');
+            details.appendChild(breoning);
+            breoning.innerHTML = el.breoning;
+            var attack_speed = document.createElement('div');
+            details.appendChild(attack_speed);
+            attack_speed.innerHTML = el.attack_speed;
+            var time_of_targeting = document.createElement('div');
+            details.appendChild(time_of_targeting);
+            time_of_targeting.innerHTML = el.time_of_targeting;
+            var ammunition = document.createElement('div');
+            details.appendChild(ammunition);
+            ammunition.innerHTML = el.ammunition;
         });
 
-        }
-	 
-  });
-    
- 
+    }
+
+});
 
